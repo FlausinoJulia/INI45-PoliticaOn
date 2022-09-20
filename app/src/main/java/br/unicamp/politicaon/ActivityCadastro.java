3b17abb1c05ffd88b0d27ccbec28109631e51eba
JULIA FLAUSINO DA SILVA <u21241@academico.cotuca.unicamp.br>
2022-09-20 09:48:30 -0300

    alteracoes no cadastro

---
+++ app/src/main/java/br/unicamp/politicaon/ActivityCadastro.java
@@ -0,0 +1,34 @@
+package br.unicamp.politicaon;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ActivityCadastro extends AppCompatActivity {
+
+    Button   btnCadastro;
+    EditText edtNome, edtEmail, edtSenha;
+
+    String nome, email, senha; // dados do user
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+
+        // associamos aos componetes do xml
+        btnCadastro = findViewById(R.id.btnCadastro);
+
+
+        // on click do btnCadastro
+        btnCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
