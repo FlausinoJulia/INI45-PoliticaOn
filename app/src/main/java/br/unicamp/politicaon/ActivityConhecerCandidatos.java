d9e0861605ccd2a382dfdc1a1f41a0e40039da0a
Julia <juliaflausino0511@gmail.com>
2022-11-06 18:20:24 -0300

    lendo csv na activity conhecer candidatos

--- app/src/main/java/br/unicamp/politicaon/ActivityConhecerCandidatos.java
+++ app/src/main/java/br/unicamp/politicaon/ActivityConhecerCandidatos.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.annotation.SuppressLint;
 import android.view.View;
@@ -12,10 +13,21 @@ import android.view.View;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.nio.Buffer;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ActivityConhecerCandidatos extends AppCompatActivity {
 
     BottomNavigationView menu;
     int idDoUsuario;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,42 +65,152 @@ public class ActivityConhecerCandidatos extends AppCompatActivity {
         });
     }
 
+    private List<Candidato> lerCsv()
+    {
+        List<Candidato> lista = new ArrayList<Candidato>();
+
+        InputStream is = getResources().openRawResource(R.raw.data);
+        BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+
+        String linha = " ";
+
+            try {
+                // lemos a primeira linha do arquivo, que contém os identificadores dos dados
+                br.readLine();
+
+                while ((linha = br.readLine())!= null)
+                {
+                    String[] tokens = linha.split(";");
+
+                    Candidato candidatoLido = new Candidato();
+
+                    candidatoLido.setNome(tokens[0]);
+
+                    if (tokens[1].length() > 0)
+                    {
+                        candidatoLido.setNumPartido(Integer.parseInt(tokens[1]));
+                    }
+                    else
+                        candidatoLido.setNumPartido(-1);
+
+                    if (tokens[2].length() > 0)
+                    {
+                        candidatoLido.setNumCandidato(Integer.parseInt(tokens[2]));
+                    }
+                    else
+                        candidatoLido.setNumCandidato(-1);
+
+                    candidatoLido.setSiglaPartido(tokens[3]);
+                    candidatoLido.setCargo(tokens[4]);
+                    candidatoLido.setCpfOuCnpj(tokens[5]);
+                    candidatoLido.setGrauInstrucao(tokens[6]);
+                    candidatoLido.setCorRaca(tokens[7]);
+
+                    if (tokens.length >= 8)
+                    {
+                        candidatoLido.setGenero(tokens[8]);
+                    }
+                    else
+                    {
+                        candidatoLido.setGenero(" ");
+                    }
+
+                    lista.add(candidatoLido);
+                }
+            } catch (IOException e) {
+                Log.wtf("ErroLeitura", "Erro ao ler o csv", e);
+                e.printStackTrace();
+            }
+
+        return lista;
+    }
+
     @SuppressLint("NonConstantResourceId")
     public void clickCargo(@NonNull View view)
     {
+        // lemos o arquivo csv
+        List<Candidato> candidatosCsv = lerCsv();
+
         switch (view.getId())
         {
             case R.id.btnPresidente:
             case R.id.setaPresidente:
+
+                // pegamos todos os registros de presidente da lista de candidatos e guardamos em uma lista separada
+                ArrayList<Candidato> presidentes = new ArrayList<Candidato>();
+
+                for (Candidato candidato : candidatosCsv)
+                {
+                    if (candidato.getCargo().equals("PRESIDENTE"))
+                        presidentes.add(candidato);
+                }
+
                 Intent mostrarPresidentes = new Intent(ActivityConhecerCandidatos.this, ActivityCargo.class);
                 mostrarPresidentes.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 mostrarPresidentes.putExtra("idDoUsuario", idDoUsuario);
-                mostrarPresidentes.putExtra("cargoEscolhido", "PRESIDENTE");
+                mostrarPresidentes.putParcelableArrayListExtra("listaPresidentes", presidentes);
                 startActivity(mostrarPresidentes);
+
                 break;
             case R.id.btnVicePresidente:
             case R.id.setaVicePresidente:
+
+                // pegamos todos os registros de vice presidentes da lista de candidatos e guardamos em uma lista separada
+                ArrayList<Candidato> vicePresidentes = new ArrayList<Candidato>();
+
+                for (Candidato candidato : candidatosCsv)
+                {
+                    if (candidato.getCargo().equals("VICE PRESIDENTE"))
+                        vicePresidentes.add(candidato);
+                }
+
                 Intent mostrarVicePre = new Intent(ActivityConhecerCandidatos.this, ActivityCargo.class);
                 mostrarVicePre.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 mostrarVicePre.putExtra("idDoUsuario", idDoUsuario);
                 mostrarVicePre.putExtra("cargoEscolhido", "VICE PRESIDENTE");
+                mostrarVicePre.putParcelableArrayListExtra("listaVices", vicePresidentes);
                 startActivity(mostrarVicePre);
+
                 break;
             case R.id.btnDeputadoFederal:
             case R.id.setaDeputadoFederal:
+
+                // pegamos todos os registros de dep. federais da lista de candidatos e guardamos em uma lista separada
+                ArrayList<Candidato> deputadosFederais = new ArrayList<Candidato>();
+
+                for (Candidato candidato : candidatosCsv)
+                {
+                    if (candidato.getCargo().equals("DEPUTADO FEDERAL"))
+                        deputadosFederais.add(candidato);
+                }
+
                 Intent mostrarDepFederal = new Intent(ActivityConhecerCandidatos.this, ActivityCargo.class);
                 mostrarDepFederal.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 mostrarDepFederal.putExtra("idDoUsuario", idDoUsuario);
                 mostrarDepFederal.putExtra("cargoEscolhido", "DEPUTADO FEDERAL");
+                mostrarDepFederal.putParcelableArrayListExtra("listaDeputadosFederais", deputadosFederais);
                 startActivity(mostrarDepFederal);
+
                 break;
             case R.id.btnSenador:
             case R.id.setaSenador:
+
+                // pegamos todos os registros de senadores da lista de candidatos e guardamos em uma lista separada
+                ArrayList<Candidato> senadores = new ArrayList<Candidato>();
+
+                for (Candidato candidato : candidatosCsv)
+                {
+                    if (candidato.getCargo().equals("SENADOR"))
+                        senadores.add(candidato);
+                }
+
                 Intent mostrarSenadores = new Intent(ActivityConhecerCandidatos.this, ActivityCargo.class);
                 mostrarSenadores.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 mostrarSenadores.putExtra("idDoUsuario", idDoUsuario);
                 mostrarSenadores.putExtra("cargoEscolhido", "SENADOR");
+                mostrarSenadores.putParcelableArrayListExtra("listaSenadores", senadores);
                 startActivity(mostrarSenadores);
+                
                 break;
         }
     }
---
+++ app/src/main/java/br/unicamp/politicaon/Candidato.java
@@ -0,0 +1,159 @@
+package br.unicamp.politicaon;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Candidato implements Parcelable
+{
+    private String nome;
+    private int numPartido;
+    private int numCandidato;
+    private String siglaPartido;
+    private String cargo;
+    private String cpfOuCnpj;
+    private String grauInstrucao;
+    private String corRaca;
+    private String genero;
+
+    public Candidato(String nome, int numPartido, int numCandidato, String siglaPartido, String cargo,
+                     String cpfOuCnpj, String grauInstrucao, String corRaca, String genero)
+    {
+        this.nome = nome;
+        this.numPartido = numPartido;
+        this.numCandidato = numCandidato;
+        this.siglaPartido = siglaPartido;
+        this.cargo = cargo;
+        this.cpfOuCnpj = cpfOuCnpj;
+        this.grauInstrucao = grauInstrucao;
+        this.corRaca = corRaca;
+        this.genero = genero;
+    }
+
+    public Candidato()
+    {
+        this.nome = " ";
+        this.numPartido = -1;
+        this.numCandidato = -1;
+        this.siglaPartido = " ";
+        this.cargo = " ";
+        this.cpfOuCnpj = " ";
+        this.grauInstrucao = " ";
+        this.corRaca = " ";
+        this.genero = " ";
+    }
+
+    protected Candidato(Parcel in) {
+        nome = in.readString();
+        numPartido = in.readInt();
+        numCandidato = in.readInt();
+        siglaPartido = in.readString();
+        cargo = in.readString();
+        cpfOuCnpj = in.readString();
+        grauInstrucao = in.readString();
+        corRaca = in.readString();
+        genero = in.readString();
+    }
+
+    public static final Creator<Candidato> CREATOR = new Creator<Candidato>() {
+        @Override
+        public Candidato createFromParcel(Parcel in) {
+            return new Candidato(in);
+        }
+
+        @Override
+        public Candidato[] newArray(int size) {
+            return new Candidato[size];
+        }
+    };
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumPartido() {
+        return numPartido;
+    }
+
+    public void setNumPartido(int numPartido) {
+        this.numPartido = numPartido;
+    }
+
+    public int getNumCandidato() {
+        return numCandidato;
+    }
+
+    public void setNumCandidato(int numCandidato) {
+        this.numCandidato = numCandidato;
+    }
+
+    public String getSiglaPartido() {
+        return siglaPartido;
+    }
+
+    public void setSiglaPartido(String siglaPartido) {
+        this.siglaPartido = siglaPartido;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getCpfOuCnpj() {
+        return cpfOuCnpj;
+    }
+
+    public void setCpfOuCnpj(String cpfOuCnpj) {
+        this.cpfOuCnpj = cpfOuCnpj;
+    }
+
+    public String getGrauInstrucao() {
+        return grauInstrucao;
+    }
+
+    public void setGrauInstrucao(String grauInstrucao) {
+        this.grauInstrucao = grauInstrucao;
+    }
+
+    public String getCorRaca() {
+        return corRaca;
+    }
+
+    public void setCorRaca(String corRaca) {
+        this.corRaca = corRaca;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    // implementando Parcelable
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(nome);
+        parcel.writeInt(numPartido);
+        parcel.writeInt(numCandidato);
+        parcel.writeString(siglaPartido);
+        parcel.writeString(cargo);
+        parcel.writeString(cpfOuCnpj);
+        parcel.writeString(grauInstrucao);
+        parcel.writeString(corRaca);
+        parcel.writeString(genero);
+    }
+}
