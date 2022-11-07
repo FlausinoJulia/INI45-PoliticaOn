cf85da349f974db059df1c66860b130682d82f57
JULIA FLAUSINO DA SILVA <u21241@academico.cotuca.unicamp.br>
2022-11-07 12:24:37 -0300

    aprender

--- app/src/main/java/br/unicamp/politicaon/Activities/ActivityAprender.java
+++ app/src/main/java/br/unicamp/politicaon/Activities/ActivityAprender.java
@@ -3,8 +3,10 @@ package br.unicamp.politicaon.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Switch;
 
 import org.imaginativeworld.whynotimagecarousel.ImageCarousel;
 import org.imaginativeworld.whynotimagecarousel.model.CarouselItem;
@@ -12,6 +14,10 @@ import org.imaginativeworld.whynotimagecarousel.model.CarouselItem;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.unicamp.politicaon.Aprender.Aprender_CargosFuncoes;
+import br.unicamp.politicaon.Aprender.Aprender_Historia;
+import br.unicamp.politicaon.Aprender.Aprender_SistemaPolitico;
+import br.unicamp.politicaon.Aprender.Aprender_TipoVoto;
 import br.unicamp.politicaon.R;
 
 public class ActivityAprender extends AppCompatActivity {
@@ -27,6 +33,13 @@ public class ActivityAprender extends AppCompatActivity {
 
         // Register lifecycle. For activity this will be lifecycle/getLifecycle() and for fragments it will be viewLifecycleOwner/getViewLifecycleOwner().
         carousel.registerLifecycle(getLifecycle());
+
+        // Teste
+        list.add(
+                new CarouselItem(
+                        "https://images.unsplash.com/photo-1534447677768-be436bb09401?w=1080"
+                )
+        );
 
         // Image drawable with caption
         list.add(
@@ -59,16 +72,47 @@ public class ActivityAprender extends AppCompatActivity {
                 )
         );
 
+
+
         carousel.setData(list);
 
+        /*
         AppCompatButton btnVoltarAprender;
+
 
         btnVoltarAprender = findViewById(R.id.btnVoltarAprender);
         btnVoltarAprender.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish(); // terminamos essa activity e voltamos para a passada
+                Intent intent = new Intent(ActivityAprender.this, ActivityInicio.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
             }
-        });
+        });*/
+    }
+
+    public void clickAprender (View view)
+    {
+        switch(view.getId()) {
+            case R.id.tema_sistemaPolitico:
+                Intent irProSisPoli = new Intent(ActivityAprender.this, Aprender_SistemaPolitico.class);
+                startActivity(irProSisPoli);
+                break;
+
+            case R.id.tema_cargosFuncoes:
+                Intent irProCargosFuncoes = new Intent(ActivityAprender.this, Aprender_CargosFuncoes.class);
+                startActivity(irProCargosFuncoes);
+                break;
+
+            case R.id.tema_tipoVoto:
+                Intent irProTipoVoto = new Intent(ActivityAprender.this, Aprender_TipoVoto.class);
+                startActivity(irProTipoVoto);
+                break;
+
+            case R.id.tema_historia:
+                Intent irProHistoria = new Intent(ActivityAprender.this, Aprender_Historia.class);
+                startActivity(irProHistoria);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/br/unicamp/politicaon/Aprender/Aprender_TipoVoto.java
@@ -0,0 +1,64 @@
+package br.unicamp.politicaon.Aprender;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import br.unicamp.politicaon.Activities.ActivityConhecerCandidatos;
+import br.unicamp.politicaon.Activities.ActivityInicio;
+import br.unicamp.politicaon.R;
+
+public class Aprender_TipoVoto extends AppCompatActivity {
+
+    BottomNavigationView menu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aprender_tipo_voto);
+
+        AppCompatButton arrow_left = findViewById(R.id.arrow_left);
+        arrow_left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish(); // terminamos essa activity e voltamos para a passada
+            }
+        });
+
+        menu = findViewById(R.id.menu_horizontal);
+        menu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.aprender:
+                        finish();
+                        break;
+                    case R.id.monitorar:
+                        //Intent irProMonitorar = new Intent(ActivityInicio.this, ActivityMonitorar.class);
+                        //irProMonitorar.putExtra("idDoUsuario", idDoUsuario);
+                        break;
+                    case R.id.inicio:
+                        Intent irProInicio = new Intent(Aprender_TipoVoto.this, ActivityInicio.class);
+                        irProInicio.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(irProInicio);
+                        break;
+                    case R.id.conhecer:
+                        Intent irProConhecer = new Intent(Aprender_TipoVoto.this, ActivityConhecerCandidatos.class);
+                        startActivity(irProConhecer);
+                        break;
+                }
+
+                return true;
+            }
+        });
+    }
+}
